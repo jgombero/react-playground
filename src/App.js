8a721d06087b236833effe63ae8dab15615d3c18
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-05-03 18:31:36 -0700

    Render useEffect Example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import State from "./components/State";
 import HR from "./components/HorizontalRule";
+import Effect from "./components/Effect";
 
 function App() {
   return (
     <>
       <State />
+      <HR />
+      <Effect />
       <HR />
     </>
   );
