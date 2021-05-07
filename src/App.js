ff5856f9a865520d60fa5f6c0f28079aff91eb86
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-05-06 22:35:05 -0700

    Lazy load the Examples component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Router } from "@reach/router";
-import Examples from "./components/Examples";
+// import Examples from "./components/Examples";
 import NavBar from "./components/NavBar";
+
+const Examples = lazy(() => import("./components/Examples"));
 
 function App() {
   return (
     <>
       <NavBar />
-      <Router>
-        <Examples path="/examples" />
-      </Router>
+      <Suspense fallback={<h1>loading route ...</h1>}>
+        <Router>
+          <Examples path="/examples" />
+        </Router>
+      </Suspense>
     </>
   );
 }
