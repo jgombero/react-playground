ad8c80fb647599476f5692c174926fe391b9d162
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-05-06 21:12:42 -0700

    Add weird css to navbar using emotion

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "@reach/router";
-import { css } from "@emotion/react";
+import { css, keyframes } from "@emotion/react";
+import colors from "../colors";
+
+const spin = keyframes`
+  to {
+    transform: rotate(360deg);
+  }
+`;
 
 const NavBar = () => {
+  const [goodColor, setGoodColor] = useState(true);
+  const color = goodColor ? "hotpink" : "limegreen";
+
   return (
     <header
+      onClick={() => setGoodColor(!goodColor)}
       css={css`
-        background-color: hotpink;
+        background-color: ${color};
         padding: 15px;
+        display: flex;
+        justify-content: space-between;
       `}
     >
       <Link to="/">Home</Link>
-      <span role="img" aria-label="logo">
+      <span
+        css={css`
+          font-size: 60px;
+          display: inline-block;
+          animation: 1s ${spin} linear;
+          animation-iteration-count: 5;
+
+          &:hover {
+            animation: 1s ${spin} linear infinite reverse;
+            text-decoration: underline ${colors.dark};
+          }
+        `}
+        role="img"
+        aria-label="logo"
+      >
         🥸
       </span>
     </header>
