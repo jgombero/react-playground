b8c4e6e8c4c88fc396119eb2cd4b551f52c2c400
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-05-06 22:35:45 -0700

    Use combination of styled component format

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,12 +2,7 @@ import React, { useState } from "react";
 import { Link } from "@reach/router";
 import { css, keyframes } from "@emotion/react";
 import colors from "../colors";
-
-const spin = keyframes`
-  to {
-    transform: rotate(360deg);
-  }
-`;
+import styled from "@emotion/styled";
 
 const NavBar = () => {
   const [goodColor, setGoodColor] = useState(true);
@@ -23,43 +18,40 @@ const NavBar = () => {
         justify-content: space-between;
       `}
     >
-      <div>
-        <Link
-          css={css`
-            padding: 15px;
-          `}
-          to="/"
-        >
-          Home
-        </Link>
-        <Link
-          css={css`
-            padding: 15px;
-          `}
-          to="examples"
-        >
-          Examples
-        </Link>
+      <div style={{ display: "flex" }}>
+        <Div>
+          <Link to="/">Home</Link>
+        </Div>
+        <Div>
+          <Link to="examples">Examples</Link>
+        </Div>
       </div>
-      <span
-        css={css`
-          font-size: 60px;
-          display: inline-block;
-          animation: 1s ${spin} linear;
-          animation-iteration-count: 5;
-
-          &:hover {
-            animation: 1s ${spin} linear infinite reverse;
-            text-decoration: underline ${colors.dark};
-          }
-        `}
-        role="img"
-        aria-label="logo"
-      >
+      <Span role="img" aria-label="logo">
         🥸
-      </span>
+      </Span>
     </header>
   );
 };
+
+const spin = keyframes`
+    to {
+      transform: rotate(360deg);
+    }
+  `;
+
+const Div = styled.div`
+  padding: 10px;
+`;
+
+const Span = styled.span`
+  font-size: 60px;
+  display: inline-block;
+  animation: 1s ${spin} linear;
+  animation-iteration-count: 5;
+  &:hover {
+    animation: 1s ${spin} linear infinite reverse;
+    text-decoration: underline ${colors.dark};
+  }
+`;
 
 export default NavBar;
