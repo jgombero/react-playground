8e28e0b6fff8ed5d08cd5bc78236aa55dcd4781c
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-05-06 21:34:12 -0700

    Extract example components into one, add router

--- src/App.js
+++ src/App.js
@@ -1,48 +1,15 @@
 import React from "react";
-import State from "./components/State";
-import HR from "./components/HorizontalRule";
-import Effect from "./components/Effect";
-import Context from "./components/Context";
-import Ref from "./components/Ref";
-import Reducer from "./components/Reducer";
-import Memo from "./components/Memo";
-import Callback from "./components/Callback";
-import Layout from "./components/Layout";
-import ImperativeHandle from "./components/ImperativeHandle";
+import { Router } from "@reach/router";
+import Examples from "./components/Examples";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
       <NavBar />
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          textAlign: "center",
-        }}
-      >
-        <State />
-        <HR />
-        <Effect />
-        <HR />
-        <Context />
-        <HR />
-        <Ref />
-        <HR />
-        <Reducer />
-        <HR />
-        <Memo />
-        <HR />
-        <Callback />
-        <HR />
-        <Layout />
-        <HR />
-        <ImperativeHandle />
-        <HR />
-      </div>
+      <Router>
+        <Examples path="/examples" />
+      </Router>
     </>
   );
 }
